ea51469ae1d55daa1e6b571a324d7cb63b7bc521
Hyun Seok <hskshin@umich.edu>
2021-11-16 21:21:45 -0500

    lecture1-2 complete

---
+++ header1.cpp
@@ -0,0 +1,9 @@
+#include "header1.h"
+
+namespace header1{
+    float pi = 3.14;
+    int foo(){
+        return 3;
+    };
+    void bar() {};
+}
---
+++ lecture1-2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "header1.h"
+
+using namespace header1;
+
+int main() {
+    std::cout << "Hello, World!" << foo() << std::endl;
+    return 0;
+}
